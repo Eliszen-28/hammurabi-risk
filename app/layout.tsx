3b8307b92357c5ec8b4b4c75d7995d815668f544
Elisa A <Eliszen@Cesars-MacBook-Pro.local>
2026-08-24 12:27:41 -0600

    Align risk section with brand palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,7 @@ import "./map-app-logos.css";
 import "./brand-slogan.css";
 import "./sales-inline-trigger.css";
 import "./incidents-elegant.css";
+import "./risk-section-brand.css";
 import SalesAssistant from "./SalesAssistant";
 import CookieConsent from "./CookieConsent";
 
